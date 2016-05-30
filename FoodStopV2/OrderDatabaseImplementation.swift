e7dd7ce6a7a5ca60760b15db3a7e546e40bbb72a
Timothy <ttkim@wesleyan.edu>
2016-05-30 15:35:52 -0400

    Iteration 1 - added orderdatabaseimplementation

--- FoodStopV2/OrderDatabaseImplementation.swift
+++ FoodStopV2/OrderDatabaseImplementation.swift
@@ -8,10 +8,42 @@
 
 import SQLite
 
-let db = try! Connection("orderlist.db.sqlite3")
+let path = NSSearchPathForDirectoriesInDomains(
+    .DocumentDirectory, .UserDomainMask, true
+    ).first!
 
-let orders = Table("orders")
-let id = Expression<Int64>("id")
-let foodService = Expression<String>("foodService")
-let destination = Expression<String>("destination")
-let foodChoice = Expression<String>("foodChoice")
+let db = try! Connection("\(path)/db.sqlite3")
+//let db = try! Connection("orderlist.db.sqlite3")
+
+class OrderDatabaseImplementation{
+    
+    let orders = Table("orders")
+    let id = Expression<Int64>("id")
+    let foodService = Expression<String>("foodService")
+    let destination = Expression<String>("destination")
+    let foodChoice = Expression<String>("foodChoice")
+    
+    func createOrdersTable(){
+        
+        try! db.run(orders.create { t in
+            t.column(id, primaryKey: .Autoincrement)
+            t.column(foodService)
+            t.column(destination)
+            t.column(foodChoice)
+            })
+    }
+    
+    func insertOrder(){
+        let insert = orders.insert(foodService <- "Summerfields", destination <- "Butterfields B")
+        let rowid = try! db.run(insert)
+    }
+    
+    func selectFromOrders(){
+        for order in try! db.prepare(orders){
+            print("id: \(order[id]), foodService: \(order[foodService]), destination: \(order[destination])")
+        }
+    }
+    
+    
+    
+}
