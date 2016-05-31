569dd6848e698e1b37697b244f3800115c679a6a
Timothy <ttkim@wesleyan.edu>
2016-05-31 14:33:25 -0400

    Iteration 1 - added a basic Order model and tests

---
+++ FoodStopV2/Order.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  FoodStopV2
+//
+//  Created by Tim Kim on 5/30/16.
+//  Copyright © 2016 FoodStop. All rights reserved.
+//
+
+import UIKit
+
+class Order {
+    var foodService: String
+    var destination: String
+    var foodChoice: String
+    
+    init?(foodService: String, destination: String, foodChoice: String){
+        self.foodService = foodService
+        self.destination = destination
+        self.foodChoice = foodChoice
+        
+        if foodService.isEmpty || destination.isEmpty || foodChoice.isEmpty{
+            return nil
+        }
+    }
+}
