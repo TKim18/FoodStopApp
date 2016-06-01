111ebb7824805df290635eba27377585518c8cd4
Timothy <ttkim@wesleyan.edu>
2016-05-31 22:17:51 -0400

    Iteration 1 - database connection works and updated api

--- FoodStopV2/OrderDatabaseImplementation.swift
+++ FoodStopV2/OrderDatabaseImplementation.swift
@@ -24,25 +24,63 @@ class OrderDatabaseImplementation{
     let foodChoice = Expression<String>("foodChoice")
     
     func createOrdersTable(){
-        
-        try! db.run(orders.create { t in
-            t.column(id, primaryKey: .Autoincrement)
-            t.column(foodService)
-            t.column(destination)
-            t.column(foodChoice)
-            })
-    }
-    
-    func insertOrder(){
-        let insert = orders.insert(foodService <- "Summerfields", destination <- "Butterfields B")
-        let rowid = try! db.run(insert)
-    }
-    
-    func selectFromOrders(){
-        for order in try! db.prepare(orders){
-            print("id: \(order[id]), foodService: \(order[foodService]), destination: \(order[destination])")
+        do {
+            try db.run(orders.create { t in
+                t.column(id, primaryKey: .Autoincrement)
+                t.column(foodService)
+                t.column(destination)
+                t.column(foodChoice)
+                })
+            print("Successfully created table")
+        } catch {
+            print("Database creation failed: \(error)")
         }
     }
+    
+    func insertOrder(fS: String, dest: String, fC: String) -> Int64{
+        let insert = orders.insert(foodService <- fS, destination <- dest, foodChoice <- fC)
+        var row: Int64 = 0
+        do {
+            let rowid = try db.run(insert)
+            row = rowid
+            print("inserted id: \(rowid)")
+        } catch {
+            print("insertion failed: \(error)")
+        }
+        return row
+    }
+    
+    func selectAllOrders() -> String{
+        var temp: String = ""
+        for order in try! db.prepare(orders){
+            let curr = "id: \(order[id]), foodService: \(order[foodService]), destination: \(order[destination])"
+            temp += curr + "\r\n"
+            print(curr)
+        }
+        return temp
+    }
+    
+    
+    func selectOneOrder() -> String {
+        let curr = try db.pluck(orders)![destination]
+        return curr
+    }
+    
+    //use more as template
+    func updateOrder(curr_id: Int64, newValue: String){
+        let order = orders.filter(id == curr_id)
+        do {
+            if try db.run(order.update(destination <- newValue)) > 0 {
+                print("updated alice")
+            } else {
+                print("alice not found")
+            }
+        } catch {
+            print("update failed: \(error)")
+        }
+    }
+    
+    
     
     
     
--- FoodStopV2/ViewController.swift
+++ FoodStopV2/ViewController.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var resultsTextView : UITextView!
+    @IBOutlet var someTextField : UITextField!
+    
+    @IBAction func getRowNumber(sender : AnyObject){
+        someTextField.text = "heyhey"
+    }
+    
+    @IBAction func askForValue(sender : AnyObject){
+        let db = OrderDatabaseImplementation()
+        //db.createOrdersTable()
+        db.insertOrder("WesWings", dest: "Butterfields C", fC: "Quesadilla")
+        let temp:String = db.selectAllOrders()
+        resultsTextView.text = temp
+        someTextField.text = db.selectOneOrder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
