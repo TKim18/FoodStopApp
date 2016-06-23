3612c87b6700a7d3b7b9f3f1ee8aeca32f57b0e3
Timothy <ttkim@wesleyan.edu>
2016-06-22 20:09:20 -0400

    Iteration 2 - template storyboard set out for testing

---
+++ FoodStopV2/LoginViewController.swift
@@ -0,0 +1,113 @@
+//
+//  LoginViewController.swift
+//  FoodStopV2
+//
+//  Created by Tim Kim on 6/22/16.
+//  Copyright © 2016 FoodStop. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField : UITextField!
+    @IBOutlet var passwordTextField : UITextField!
+    @IBOutlet var messageLabel : UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passwordTextField.secureTextEntry = true
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        var value: Bool = true
+        if identifier == "LoginSegue" { // you define it in the storyboard (click on the segue, then Attributes' inspector > Identifier
+            if usernameTextField.text!.isEmpty{
+                value = false
+                messageLabel.text = "Please input a valid username"
+            }
+            else if passwordTextField.text!.isEmpty{
+                value = false
+                messageLabel.text = "Please input a valid password"
+            }
+            else{
+                do {
+                    let currUser = try PFUser.logInWithUsername(usernameTextField.text!, password: passwordTextField.text!)
+                    //performing synchronous login
+                    if currUser.username == nil {
+                        value = false
+                        messageLabel.text = "Incorrect email/password"
+                        print ("nothing should happen...")
+                    } else{
+                        print ("login!")
+                    }
+                }
+                catch {
+                    value = false
+                    print ("Something went wrong")
+                }
+            }
+            
+            if (value == false){
+                //if (self.usernameTextField.text!.isEmpty == true) {
+                print("*** NOPE, segue wont occur")
+                return false
+            }
+            else {
+                print("*** YEP, segue will occur")
+            }
+        }
+        
+        // by default, transition
+        return true
+    }
+    
+    @IBAction func loginButton(sender : AnyObject){
+        self.shouldPerformSegueWithIdentifier("VerificationSegue", sender: nil)
+        //adding a conditional to the segue
+    }
+    /*
+     override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+     var value : Bool = true
+     if identifier == "VerificationSegue" { // you define it in the storyboard (click on the segue, then Attributes' inspector > Identifier
+     
+     PFUser.logInWithUsernameInBackground(usernameTextField.text!, password: passwordTextField.text!){ user, error in
+     if (user == nil) {
+     value = false
+     print(user)
+     print("*** NOPE, segue wont occur")
+     return
+     }
+     else {
+     print("*** YEP, segue will occur")
+     }
+     }
+     
+     }
+     
+     return value
+     // by default, transition
+     }
+     
+     @IBAction func loginButton(sender : AnyObject){
+     self.shouldPerformSegueWithIdentifier("VerificationSegue", sender: nil)
+     }
+     
+     PFUser.logInWithUsernameInBackground(usernameTextField.text!, password: passwordTextField.text!) { user, error in
+     if user != nil {
+     self.shouldPerformSegueWithIdentifier("VerificationSegue", sender: nil)
+     } else if let error = error {
+     print("Error: \(error)")
+     }
+     }
+     */
+    
+    
+}
---
+++ FoodStopV2/RegisterViewController.swift
@@ -0,0 +1,119 @@
+//
+//  RegisterViewController.swift
+//  FoodStopV2
+//
+//  Created by Tim Kim on 6/22/16.
+//  Copyright © 2016 FoodStop. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        passwordTextField.secureTextEntry = true
+        verifyTextField.secureTextEntry = true
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet var fullnameTextField : UITextField!
+    @IBOutlet var usernameTextField : UITextField!
+    @IBOutlet var emailTextField : UITextField!
+    @IBOutlet var passwordTextField : UITextField!
+    @IBOutlet var verifyTextField : UITextField!
+    @IBOutlet var messageLabel: UITextView!
+    
+    @IBAction func registerButton(sender : AnyObject){
+        shouldPerformSegueWithIdentifier("RegisterSegue", sender: nil)
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        var value: Bool = true
+        if identifier == "RegisterSegue" { // you define it in the storyboard (click on the segue, then Attributes' inspector > Identifier
+            
+            let fullname = fullnameTextField.text
+            let username = usernameTextField.text
+            let email = emailTextField.text
+            let password = passwordTextField.text
+            
+            if (fullname == ""){
+                value = false
+                messageLabel.text = "Please input a valid name."
+            }
+            else if(username == ""){
+                value = false
+                messageLabel.text = "Please input a valid username."
+            }
+            else if(email == ""){
+                value = false
+                messageLabel.text = "Please input a valid email."
+            }
+            else if(password == ""){
+                value = false
+                messageLabel.text = "Please input a valid password."
+            }
+            else if(password != verifyTextField.text){
+                value = false
+                messageLabel.text = "Your password and verification do not match."
+            }
+            else{
+                addUser(fullname!, regUser: username!, regEmail: email!, regPassword: password!)
+                print("Everything seems good")
+            }
+            
+            if (value == false){
+                print(value)
+                print("*** NOPE, segue wont occur")
+                return false
+            }
+            else {
+                print(value)
+                print("*** YEP, segue will occur")
+            }
+        }
+        
+        // by default, transition
+        return false
+    }
+    
+    
+    func addUser(regName: String, regUser: String, regEmail: String, regPassword: String){
+        let user = PFUser()
+        user.username = regUser
+        user.password = regPassword
+        user.signUpInBackgroundWithBlock { (success: Bool, error: NSError?)  -> Void in
+            if success {
+                self.messageLabel.text = "You are now signed up!";
+                self.performSegueWithIdentifier("RegisterSegue", sender: nil)
+                print("User Uploaded")
+            } else {
+                print("Error: \(error)")
+            }
+        }
+        let match = PFObject(className: "Matching")
+        match.setObject(regName, forKey: "fullname")
+        match.setObject(regUser, forKey: "username")
+        match.setObject(regEmail, forKey: "emailAddress")
+        match.setObject(false, forKey: "hasMatched")
+        match.setObject("", forKey: "matchedUser")
+        match.saveInBackgroundWithBlock { (succeeded, error) -> Void in
+            if succeeded {
+                print("Match Uploaded")
+            } else {
+                print("Error: \(error)")
+            }
+        }
+        
+    }
+    
+}
+
+
+
